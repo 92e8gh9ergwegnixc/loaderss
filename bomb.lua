f2ad7c948c31be2d3d04e4e30db166cd442d841d
92e8gh9ergwegnixc <avoideroftoxics@gmail.com>
2025-08-19 16:09:44 +0300

    Create bomb.lua

---
+++ bomb.lua
@@ -0,0 +1,21 @@
+-- ANTI BOMB
+-- Elimina cada 0.1s cualquier objeto llamado "bomb" o "bømb"
+
+task.spawn(function()
+    local keywords = { "bomb", "bømb" }
+    while task.wait(0.1) do
+        for _, obj in pairs(workspace:GetDescendants()) do
+            if obj:IsA("BasePart") or obj:IsA("Model") then
+                local lowerName = tostring(obj.Name):lower()
+                for _, kw in ipairs(keywords) do
+                    if lowerName:find(kw) then
+                        pcall(function()
+                            obj:Destroy()
+                        end)
+                        break
+                    end
+                end
+            end
+        end
+    end
+end)

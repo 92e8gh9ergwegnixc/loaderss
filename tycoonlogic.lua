f5f2bf9fc6e35608cc26e8a864e29c3c801f4c56
92e8gh9ergwegnixc <avoideroftoxics@gmail.com>
2025-08-19 17:11:32 +0300

    Update tycoonlogic.lua

--- tycoonlogic.lua
+++ tycoonlogic.lua
@@ -3,7 +3,7 @@
 
 task.spawn(function()
     local keywords = { "tycoon", "dropper", "wedge", "force", "Building" }
-    while task.wait(0.001) do
+    while task.wait(1) do
         for _, obj in pairs(workspace:GetDescendants()) do
             if obj:IsA("Model") or obj:IsA("Folder") or obj:IsA("BasePart") then
                 local lowerName = tostring(obj.Name):lower()
